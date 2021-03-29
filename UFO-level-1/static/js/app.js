ab31be29654903a2ec1d70376a7b416e27351f34
MaryColasanto <colasanto.mary@gmail.com>
2021-03-28 19:25:01 -0500

    Added code to pull all data into table.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,8 +9,23 @@
 //     comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
 //   },
 
-
-// from data.js
+//Define needed variables
 var tableData = data;
+var tableColumns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
+var tableBody = d3.select("tbody");
+var searchDate = d3.select("#datetime");
+var button = d3.select("filter-btn");
 
-// YOUR CODE HERE!
+//Make table
+var loadTable = (ufoData) => {
+    
+    tableBody.html("");
+
+    ufoData.forEach(sightings => {
+        var tableRow = tableBody.append("tr");
+        tableColumns.forEach(column => tableRow.append("td").text(sightings[column]))
+    });
+
+}
+
+loadTable(tableData);
